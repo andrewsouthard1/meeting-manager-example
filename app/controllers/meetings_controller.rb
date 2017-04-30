b421939de60ebc9cd259a9835882cf1635624112
Brian <riceb53@gmail.com>
2017-04-30 16:02:53 -0400

    add new and create actions

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -12,4 +12,26 @@ class MeetingsController < ApplicationController
     @meeting = Meeting.find_by(id: params[:id])
     render 'show.html.erb'
   end
+
+  def new
+    @meeting = Meeting.new
+    render 'new.html.erb'
+  end
+
+  def create
+    # make the new meeting
+    @meeting = Meeting.new(
+      notes: params[:notes] ,
+      address: params[:address] ,
+      name: params[:name] ,
+      start_time: params[:start_time] ,
+      end_time: params[:end_time]
+    )
+    if @meeting.save
+      flash[:success] = "You totally just made a new meeting"
+      redirect_to "/meetings/#{@meeting.id}"
+    else
+      render 'new.html.erb'
+    end
+  end
 end
